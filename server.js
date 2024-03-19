b80fcac200241c81ca6b6f04951676e2728514f3
SmartWavesCafe <smartwavescafe@gmail.com>
2024-03-19 19:16:08 +0100

    a

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -10,6 +11,21 @@ app.get('/', (req, res) => {
     console.log(error)
   }
 })
+
+mongoose.connect('mongodb+srv://admin:admin@rakocziradio.4wm3zxb.mongodb.net/?retryWrites=true&w=majority&appName=RakocziRadio')
+.then(() => {
+  console.log('Csatlakozva az adatbázishoz!')
+})
+
+const uzenetSchema = new mongoose.Schema({
+  from: String,
+  to: { type: String, default: '*' },
+  message: String,
+  date: { type: Date, default: Date.now },
+})
+
+const Uzenet = mongoose.model('Üzenet', uzenetSchema, 'messages')
+
 
 app.get("/musorlista", (req, res) => {
   res.sendFile(__dirname + '/public/musorlista.html')
@@ -27,6 +43,28 @@ app.get("/fooldal", (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 });
 
+app.post("/sendMessage", async (req, res) => {
+  try {
+    const { to, from, msg } = req.body
+    const date = Date.now()
+    const newMsg = new Uzenet({ from, to, msg, date })
+    await newMsg.save()
+    res.redirect('/')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal Server Error')
+  }
+})
+
+app.get("/messages", async (req, res) => {
+    try {
+      const msgs = await Uzenet.find()
+      res.json(msgs)
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('Internal Server Error')
+    }
+})
 app.listen(port, () => {
-  console.log(`Express app listening on port ${port}`)
+  console.log(`Szerver online a ${port}-es porton.`)
 })
